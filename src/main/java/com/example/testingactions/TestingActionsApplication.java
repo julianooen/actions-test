446db609384086c3670e72848da1cb004ebf3e7b
juliano.ferreira <juliano.ferreira@ilegra.com>
2025-01-07 00:52:01 -0300

    1st

---
+++ src/main/java/com/example/testingactions/TestingActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingActionsApplication.class, args);
+	}
+
+}
